30d19061dd99557c58739b66fe81465789775a1a
Thalisson <thalissonmt@gmail.com>
2022-08-14 22:19:54 -0300

    inicializacao do sprint

---
+++ implementacao/src/main/java/ufpe/cin/easyfix/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package ufpe.cin.easyfix.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ implementacao/src/test/java/ufpe/cin/easyfix/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ufpe.cin.easyfix.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
