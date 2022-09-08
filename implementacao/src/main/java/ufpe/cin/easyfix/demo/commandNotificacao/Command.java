98981e07d8194982eafe404e7a8f96240f97d96d
Thalisson <thalissonmt@gmail.com>
2022-09-08 19:40:27 -0300

    ajuste no decorator

--- implementacao/src/main/java/ufpe/cin/easyfix/demo/commandNotificacao/Command.java
+++
@@ -1,7 +0,0 @@
-package ufpe.cin.easyfix.demo.commandNotificacao;
-
-import ufpe.cin.easyfix.demo.subsistemaNotificacao.Notificacao;
-
-public interface Command {
-    public void execute(Notificacao notificacao);
-}
--- implementacao/src/main/java/ufpe/cin/easyfix/demo/subsistemaNotificacao/SlackNotificacaoDecorator.java
+++ implementacao/src/main/java/ufpe/cin/easyfix/demo/subsistemaNotificacao/SlackNotificacaoDecorator.java
@@ -1,13 +1,15 @@
 package ufpe.cin.easyfix.demo.subsistemaNotificacao;
 
-public class SlackNotificacaoDecorator extends NotificacaoDecorator {
+public class SlackNotificacaoDecorator implements ISubsistemaNotificacao {
+    private ISubsistemaNotificacao wrapee;
 
     public SlackNotificacaoDecorator(ISubsistemaNotificacao wrapee) {
-        super(wrapee);
+        this.wrapee = wrapee;
     }
+
     @Override
     public void enviarNotificacao(Notificacao notificacao) {
         System.out.println("Notificacao enviada através do slack para " + notificacao.getDestino() + " com mensagem:\n" + notificacao.getMensagem());
-        super.enviarNotificacao(notificacao);
+        wrapee.enviarNotificacao(notificacao);
     }
 }
